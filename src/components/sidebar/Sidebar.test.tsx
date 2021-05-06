1b837f01dd838814c1747ac730dfb3a0b7c1c247
tonyw0527 <tonyw0527@gmail.com>
2021-05-06 23:07:57 +0900

    Add jest, testing-library and etc for testing & test unit test

---
+++ src/components/sidebar/Sidebar.test.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import Sidebar from "./Sidebar"
+
+describe("Sidebar", () => {
+  test("renders Sidebar", () => {
+    const { container } = render(<Sidebar />)
+
+    expect(container).toHaveTextContent("test")
+  })
+})
