0ca1ae0966a3a1d90a39ffa7ba16f5817b59f951
tonyw0527 <tonyw0527@gmail.com>
2021-05-07 02:41:06 +0900

    Updates

---
+++ component---src-pages-404-js-3117e9914586acd18442.js
@@ -0,0 +1,2 @@
+(self.webpackChunktonyw_west_blog=self.webpackChunktonyw_west_blog||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var a=n(7294),l=n(2300),o=n(3751);e.default=function(t){var e=t.data,n=t.location,s=e.site.siteMetadata.title;return a.createElement(l.Z,{location:n,title:s},a.createElement(o.Z,{title:"404: Not Found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3117e9914586acd18442.js.map
